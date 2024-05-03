48a46aaa11b113eb470e8a99f113593f15f2a996
Md Mostaq Muzahid Moin <138385290+Mostaq27@users.noreply.github.com>
2024-05-03 06:17:42 +0600

    Update Products.jsx

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,7 +3,7 @@ import ProductCard from "../ProductCard/ProductCard";
 const Products = () => {
   return (
     <div>
-      <h1 className="font-bold text-4xl text-center py-10">This is Products</h1>
+      <h1 className="font-bold text-4xl text-center py-10">Our Products</h1>
       <ProductCard></ProductCard>
     </div>
   );
