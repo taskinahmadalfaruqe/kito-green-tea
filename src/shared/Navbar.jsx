bc62dbb88a14728a288dfde0261803318aed24e1
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2024-05-08 01:34:14 +0600

    [logo Issue]: Update Logo Issue.

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -57,7 +57,7 @@ const Navbar = () => {
             </div>
             <Link to="/">
               <div className="flex justify-start items-center lg:gap-2 text-base lg:text-2xl  text-White_Color">
-                <img className="hidden md:visible w-16 h-12 rounded-2xl"
+                <img className="hidden md:flex w-16 h-12 rounded-2xl"
                   src={logo} alt="Logo" />
                   <span>E Shop BD</span>
               </div>
